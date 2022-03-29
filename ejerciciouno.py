97eab2671df4d83ce5799f0f2ae086f13ca97a7c
anamornig <anamoruja@gmail.com>
2022-03-29 17:05:13 +0100

    git commit

---
+++ ejerciciouno.py
@@ -0,0 +1,7 @@
+nota_uno= float(input("nota uno: "))
+nota_dos= float(input("nota dos: "))
+nota_tres= float(input("nota tres: "))
+
+nota_final= (15* nota_uno + 35* nota_dos + 50* nota_tres)/100
+
+print("promedio:", nota_final)
